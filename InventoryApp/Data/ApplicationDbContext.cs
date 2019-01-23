e8e0901329dd47fa3edae23ef37ea3da8be5af5c
jcoole15 <crashmatusow@gmail.com>
2019-01-22 23:16:41 -0600

    Identity Setup

--- InventoryApp/Data/ApplicationDbContext.cs
+++ InventoryApp/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using InventoryApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace InventoryApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<InventoryUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
